4e963bb59bc56f3e6b44d170b10f6a365c5b8a8e
yuvasrik7 <yuvasrik007@gmail.com>
2025-08-26 18:29:09 +0530

    Create watch.js

---
+++ watch.js
@@ -0,0 +1,21 @@
+let display = document.getElementById("time");
+let timer = null;
+let starttime = 0;
+let elapsedtime = 0;
+let isrunning = false;
+
+function start() {
+    if (!isrunning) {
+        starttime = Date.now() - elapsedtime;
+        timer = setInterval(update, 10);
+        isrunning = true;
+    }
+}
+
+function stop() {
+    if (isrunning) {
+        clearInterval(timer);
+        elapsedtime = Date.now() - starttime;
+        isrunning = false;
+    }
+}
